c491084ee6c179789c3038276dc24b80ec7fc4c4
Jonathan Golden <jegolden@wpi.edu>
2025-03-20 16:21:44 -0400

    Added SQL Data Dump

--- ATMSystem/obj/Debug/net9.0/ATMSystem.AssemblyInfo.cs
+++ ATMSystem/obj/Debug/net9.0/ATMSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATMSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0f91b900860d580cf911454333559aa4798d57e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+665cb90f872efdeaf816cae0f9c9f1ad8285acfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATMSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATMSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
